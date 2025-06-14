e08a8a76adae5724d1a71c5f01948640312a9d2b
Hude Intres <hudeintres@gmail.com>
2025-06-14 16:20:11 +0000

    gitignore script

--- scripts/query-jsonrpc.mjs
+++ scripts/query-jsonrpc.mjs
@@ -1,9 +1,8 @@
 import { AlkanesRpc } from "../lib/rpc.js";
 
-const rpc = new AlkanesRpc({ baseUrl: 'http://localhost:8091' });
+// const rpc = new AlkanesRpc({ baseUrl: 'https://alkanes.pyrosec.is' });
 const prod_rpc = new AlkanesRpc({ baseUrl: 'https://mainnet.sandshrew.io/v2/lasereyes' });
 
-// Query protorunes balance
 // const balance = await rpc.protorunesbyaddress({
 //   address: 'bc1pr8vjq0fk89f5sw3r4n9scrasvw7kaud9akhzw57c3ygycsjspvvseyjcma',
 //   protocolTag: 1n,
@@ -18,25 +17,26 @@ const prod_rpc = new AlkanesRpc({ baseUrl: 'https://mainnet.sandshrew.io/v2/lase
 // });
 
 // console.log('Protorunes balance:', prod_balance);
-let tx = 600n;
-// // Simulate transaction
-const simulation = await rpc.simulate({
-  alkanes: [],
-  block: '',
-  transaction: '',
-  height: 1000000n,
-  txindex: 0,
-  target: {
-    block: 2n,
-    tx: tx,
-  },
-  inputs: [99n],
-  pointer: 0,
-  refundPointer: 0,
-  vout: 0,
-});
+// let tx = 26160n;
+// // // Simulate transaction
+// const simulation = await rpc.simulate({
+//   alkanes: [],
+//   block: '',
+//   transaction: '',
+//   height: 1000000n,
+//   txindex: 0,
+//   target: {
+//     block: 2n,
+//     tx: tx,
+//   },
+//   inputs: [99n],
+//   pointer: 0,
+//   refundPointer: 0,
+//   vout: 0,
+// });
 
-console.log('Simulation result:', simulation);
+// console.log('Simulation result:', simulation);
+// // 
 const prodsimulation = await prod_rpc.simulate({
   alkanes: [],
   block: '',
@@ -57,15 +57,17 @@ console.log('prodsimulation result:', prodsimulation);
 
 
 // diff balance: '082735FBD38C58D44087BAFB1D33584DDF669CC0C1C3D542082CC31F1B84CAC4'
-// const txid = '0E46A59CB7816C3143EFEA573E81567550E73CC9EC572A5382BE93B1B01D0A62'
+// const txid = '000122924201B9D5F617524CD1C4DA991A70388575E6BE02B5BCFDF1305E2DD1' //96. minted out. but new indexer minted it here
+// const txid = 'CCDC45DBF0427B63697CE0914A769D47F455CA221C6571F5709D6280F40A280C' //92
+const txid = 'FB5CBE346E8144DF3A244BC5CB34643FB792CCCA7B38B18ADC69CDB76C6E3A60' //92
 
-// const balance = await rpc.protorunesbyoutpoint({
-//   txid: txid,
-//   vout: 0,
-//   protocolTag: 1n,
-// });
+const balance = await prod_rpc.protorunesbyoutpoint({
+  txid: txid,
+  vout: 0,
+  protocolTag: 1n,
+});
 
-// console.log('Protorunes balance:', balance);
+console.log('Protorunes balance:', balance);
 
 
 // const prod_balance = await prod_rpc.protorunesbyoutpoint({
@@ -76,11 +78,11 @@ console.log('prodsimulation result:', prodsimulation);
 
 // console.log('Protorunes balance:', prod_balance);
 
-// let trace = await rpc.trace({
-//   vout: 4, txid: "2460BCC3D92C061E4A098F51F2B58A3C609F15B1C918C359AC62DFD824933C0B"
+// let trace = await prod_rpc.trace({
+//   vout: 6, txid: txid
 // });
 
-// console.log('465 trace result:', JSON.stringify(trace, (key, value) =>
+// console.log('trace result:', JSON.stringify(trace, (key, value) =>
 //   typeof value === 'bigint' ? value.toString() + 'n' : value
 // ));
 
